1dc8f741422419ec80c0da6ead026bf2051728e4
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-02-27 19:05:22 +0200

    add controllers

---
+++ controllers/ArticleController.js
@@ -0,0 +1,133 @@
+import Article from "../models/Article"
+
+export const create = async (req, res) => {
+    try {
+        const doc = new Article ({
+            title: req.body.title,
+            text: req.body.text,
+            imageUrl: req.body.imageUrl,
+            tags: req.body.tags,
+            user: req.userId,
+        });
+
+        const article = await doc.save();
+
+        res.json(article);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to create article',
+        });
+    };
+};
+
+export const getAll = async (req, res) => {
+    try {
+        const articles = await Article.find().populate('user').exec();
+
+        res.json(articles)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get article',
+        });
+    };
+};
+
+export const getLastTags = async (req, res) => {
+    try {
+        const articles = await Article.find().populate('user').limit(5).exec();
+
+        const tags = articles.map((obj) => obj.tags).flat().slice(0, 5)
+
+        res.json(tags)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get tags',
+        });
+    };
+};
+
+export const getOne = async (req, res) => {
+    try {
+        const articleId = req.params.id;
+        
+        const getOneArticle = await Article.findOneAndUpdate(
+            {
+                _id: articleId,
+            },
+            {
+                $inc: { viewsCount: 1 },
+            },
+            {
+                new: true, 
+            }
+        )
+        .populate('user'); 
+
+        if (!getOneArticle) {
+            return res.status(404).json({
+                message: 'Article not found',
+            });
+        }
+
+        res.json(getOneArticle);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to get article',
+        });
+    }
+};
+
+export const remove = async (req, res) => {
+    try {
+        const articleId = req.params.id;
+
+        const deletedArticle = await Article.findByIdAndDelete(articleId);
+
+        if (!deletedArticle) {
+            return res.status(404).json({
+                message: 'Article not found',
+            });
+        }
+
+        res.json({
+            success: true,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to delete article',
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const articleId = req.params.id;
+
+        const updatedArticle = await Article.updateOne(
+            {
+                _id: articleId,
+            },
+            {
+                title: req.body.title,
+                text: req.body.text,
+                imageUrl: req.body.imageUrl,
+                user: req.body.user,
+                tags: req.body.tags,
+            }
+        );
+
+        res.json({
+            success: true,
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to update article',
+        });
+    }
+}
---
+++ controllers/UserController.js
@@ -0,0 +1,96 @@
+import jwt from 'jsonwebtoken';
+import bcrypt, { hash } from 'bcrypt';
+import gravatar from 'gravatar';
+import User from '../models/User';
+
+export const register = async (req, res) => {
+    try {
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        const avatarUrl = gravatar.url(req.body.email);
+
+        const doc = new User({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash: hash,
+            avatarUrl,
+            token: null,
+        });
+
+        const user = await doc.save();
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(userData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to register',
+        });
+    };
+};
+
+export const login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User is not found',
+            });
+        }
+        
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+        if (!isValidPass) {
+            return res.status(404).json({
+                message: 'Wrong login or password',
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            }, 
+            'secret123',
+            {
+                expiresIn: '30d',
+            },
+        );
+        
+        user.token = token;
+        await user.save();
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Failed to login',
+        });
+    };
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.userId);
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User is not found'
+            });
+        }
+        const {passwordHash, ...userData} = user._doc;
+
+        res.json(userData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'No access',
+        });
+    };
+};
