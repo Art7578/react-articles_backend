19f33de1f336368a5276d9245d4cec7a1855af9a
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-02-27 17:13:48 +0200

    add models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        passwordHash: {
+            type: String,
+            required: true,
+        },
+        avatarUrl: {
+            type: String,
+            required: true,
+        }, 
+        token: { 
+            type: String, 
+            default: null 
+        }
+    }, 
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model('User', UserSchema);
